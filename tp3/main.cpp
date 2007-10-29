bd939e595433779dac162a4e5ae9f783a82f0256
seges.ar <seges.ar@85ae770f-1639-0410-9470-155aa9614729>
2007-10-29 16:46:04 +0000

    sube tirar señales, esta andando, pero hay que probar por las dudas.

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -7,35 +7,47 @@ using namespace std;
 void tirarSenal(long double a, long double b, Matriz<long double> &m, int n, int filaALlenar);
 void ordenarPares(long double pares[][2], int filas);
 long double menor(long double a, long double b);
+void anularRepetidos(long double pares[][2], int cantPares);
 
 int main()
 {
     long double temp = 2.5;
     int redondeo = (int)temp;
-    Matriz<long double> a(4,4);
+    Matriz<long double> m(4,16);
 
-    a.asignar(0,0,4);
-    a.asignar(0,1,3);
-    a.asignar(0,2,4);
-    a.asignar(0,3,6);
+    m.asignar(0,0,4);
+    m.asignar(0,1,3);
+    m.asignar(0,2,4);
+    m.asignar(0,3,6);
+    m.asignar(0,4,4);
 
-    a.asignar(1,0,1);
-    a.asignar(1,1,4);
-    a.asignar(1,2,0);
-    a.asignar(1,3,1);
+    m.asignar(1,0,1);
+    m.asignar(1,1,4);
+    m.asignar(1,2,0);
+    m.asignar(1,3,1);
+    m.asignar(1,4,2);
 
-    a.asignar(2,0,2);
-    a.asignar(2,1,3);
-    a.asignar(2,2,7);
-    a.asignar(2,3,8);
+    m.asignar(2,0,2);
+    m.asignar(2,1,3);
+    m.asignar(2,2,7);
+    m.asignar(2,3,8);
+    m.asignar(2,4,6);
 
-    a.asignar(3,0,9);
-    a.asignar(3,1,3);
-    a.asignar(3,2,6);
-    a.asignar(3,3,1);
+    m.asignar(3,0,9);
+    m.asignar(3,1,3);
+    m.asignar(3,2,6);
+    m.asignar(3,3,1);
+    m.asignar(3,4,1);
 
-    tirarSenal(1,2,a,4,1);
 
+    long double pend = 1;
+    long double ptoPase = 0;
+
+    cout << m << endl;
+
+    tirarSenal(pend,ptoPase,m,4,1);
+
+    cout << m << endl;
 	cout << "valor en double " << temp <<  endl;
 	cout << "valor en entero " << redondeo <<  endl;
 
@@ -46,12 +58,15 @@ int main()
 
 /*
  *  tirarSeñal: Toma una pendiente, un punto de pase para generar una recta
- *              y un n que representa la dimension de la matriz imagen
+ *              una matriz, un n que representa la dimension de la matriz
+ *              imagen y el numero de la matriz a llenar
  */
 void tirarSenal(long double a, long double b, Matriz<long double> &m, int n, int filaALlenar){
-    assert((b != 0 && a >= 0) || (b != n && a <= 0));
-    long double pares[2*n][2];
+    assert((b <= 0 && a >= 0) || (b >= n && a <= 0) || (b > 0 && b < n));
+    long double pares[2*(n+1)][2];  //2*(n+1) lo peor es que pase por la
+                                    //diagonal entonces va a haber 2*(n+1) pares
     long double temp = 0;
+    int cantPares = 0;
 
     int i = 0;
 
@@ -61,48 +76,39 @@ void tirarSenal(long double a, long double b, Matriz<long double> &m, int n, int
     // x = (y - b)/a
     // y lo guardo en pares x,y
 
-    while(i < n){
+    while(i <= n){
         long double temp = a*i + b;
 
-        if(temp < 0 || temp > n){
-            //si se paso pongo en cero para que no cuente esas distancias.
-            pares[i][0] = 0;
-            pares[i][1] = 0;
-        }
-        else
-        {
-            pares[i][0] = i;
-            pares[i][1] = temp;
+        if((temp >= 0) && (temp <= n)){
+            pares[cantPares][0] = i;
+            pares[cantPares][1] = temp;
+            cantPares++;
         }
 
         i++;
     }
 
     i = 0;
-    while(i < n){
-        if(a == 0){
-            pares[i+n][0] = 0;
-            pares[i+n][1] = 0;
-        }
-        else
-        {
-            temp = (i - b)/a;
+    while((i <= n) && (a != 0)){
+        temp = (i - b)/a;
 
-            if(temp < 0 || temp > n){
-                pares[i+n][0] = 0;
-                pares[i+n][1] = 0;
-            }
-            else
-            {
-                pares[i+n][0] = temp;
-                pares[i+n][1] = i;
-            }
+        if(temp >= 0 && temp <= n){
+            pares[cantPares][0] = temp;
+            pares[cantPares][1] = i;
+            cantPares++;
         }
         i++;
     }
 
     //ordeno por la primer componente
-    ordenarPares(pares, 2*n);
+    ordenarPares(pares, cantPares);
+
+    for(i = 0; i<cantPares; i++){
+        cout << "(" << pares[i][0] << "," << pares[i][1] << ")" << ", ";
+    }
+    cout << endl;
+    //anulo los pares repetidos
+    anularRepetidos(pares, cantPares);
 
     i = 0;
 
@@ -113,24 +119,52 @@ void tirarSenal(long double a, long double b, Matriz<long double> &m, int n, int
 
     i = 0;
 
-    while(i < (n - 1)){  //n-1 es la cantidad de cuadrados afectados
+    while(i < cantPares){
         long double x = 0;
         long double y = 0;
         long double distancia = 0;
         long double fil = 0;
         long double col = 0;
 
-        fil = (int)pares[i][0];                       //menor en x
-        col = n - 1 - (int)menor(pares[i][1], pares[i+1][1]); //menor en y, arreglo para coordenadas de la matriz
+        if( (pares[i][0] != -1)){
+            if( (pares[i+1][0] == -1) && (i+2 < cantPares) ){
+                //menor en x
+                fil = (int)pares[i][0];
+                //menor en y, arreglo para coordenadas de la matriz
+                col = n - 1 - (int)menor(pares[i][1], pares[i+2][1]);
 
-        //calculo la distancia recorrida por la señal en cada cuadrado por donde paso
-        x = pares[i][0] - pares[i+1][0];
-        y = pares[i][1] - pares[i+1][1];
-        distancia = sqrt(pow(x,2) + pow(y,2));
+                //calculo la distancia recorrida por la señal en cada cuadrado por
+                //donde paso
+                x = pares[i][0] - pares[i+2][0];
+                y = pares[i][1] - pares[i+2][1];
+                distancia = sqrt(pow(x,2) + pow(y,2));
 
-        m.asignar(filaALlenar, fil*n + col, distancia);
+                m.asignar(filaALlenar, fil*n + col, distancia);
 
-        i++;
+                i = i + 2;
+            }
+            else if(pares[i+1][0] != -1)
+            {
+                //menor en x
+                fil = (int)pares[i][0];
+                //menor en y, arreglo para coordenadas de la matriz
+                col = n - 1 - (int)menor(pares[i][1], pares[i+1][1]);
+
+                //calculo la distancia recorrida por la señal en cada cuadrado por
+                //donde paso
+                x = pares[i][0] - pares[i+1][0];
+                y = pares[i][1] - pares[i+1][1];
+                distancia = sqrt(pow(x,2) + pow(y,2));
+
+                m.asignar(filaALlenar, fil*n + col, distancia);
+
+                i++;
+            }
+            else
+                i++;
+        }
+        else
+            i = i + 2;
     }
 }
 
@@ -175,4 +209,25 @@ void ordenarPares(long double pares[][2], int filas){
         topeInf++;
 
     }
+}
+
+/*
+ * anularRepetidos: recorre el vector de pares de coordenadas y anula los
+ *                  repetidos poniendo un -1 en la coordenada x, indicando que
+ *                  esta repetida.
+ */
+void anularRepetidos(long double pares[][2], int cantPares){
+    int cant = cantPares;
+    int i = 0;
+
+    while (i < (cant - 1)){
+        if(pares[i][0] == pares[i+1][0]){
+            pares[i+1][0] = -1;
+            i = i + 2;           //salteo el anulado
+        }
+        else
+           i++;
+    }
+
+
 }
