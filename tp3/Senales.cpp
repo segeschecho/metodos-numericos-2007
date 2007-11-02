c09d76b869ca6e393ecf90b859f3fd3f0471f516
hadeskleizer <hadeskleizer@85ae770f-1639-0410-9470-155aa9614729>
2007-11-02 19:54:48 +0000

    no esta andando para imagenes de 10x10 ni de 30x30, hay que seguir testeando

--- tp3/Senales.cpp
+++ tp3/Senales.cpp
@@ -6,12 +6,16 @@
 /*          METODOS PUBLICOS         */
 /*************************************/
 
-Senales :: Senales(unsigned int dimImagen, unsigned int metodo, long double puntos1[][2], long double puntos2[][2], unsigned int cantSenales)
+Senales :: Senales(unsigned int dimImagen, unsigned int metodo)//, long double puntos1[][2], long double puntos2[][2], unsigned int cantSenales)
 {
     D = NULL;
     switch(metodo){
         case 1:
-            metodo1(dimImagen, puntos1, puntos2, cantSenales);
+//            metodo1(dimImagen, puntos1, puntos2, cantSenales);
+            metodo1(dimImagen);//, puntos1, puntos2, cantSenales);
+            break;
+        default:
+            cout << "No existe el metodo " << metodo << endl;
             break;
     }
 }
@@ -29,7 +33,7 @@ const Matriz& Senales :: MatrizSenales(void){
 /*************************************/
 /*          METODOS PRIVADOS         */
 /*************************************/
-
+/*
 void Senales :: metodo1(unsigned int dimImagen, long double puntos1[][2], long double puntos2[][2], unsigned int n)
 {
     delete D;
@@ -37,17 +41,46 @@ void Senales :: metodo1(unsigned int dimImagen, long double puntos1[][2], long d
     for(int i = 0; i < D->filas(); i++)
          tirarSenal(puntos1[i][0], puntos1[i][1], puntos2[i][0], puntos2[i][1], i);
 }
+*/
+
+void Senales :: metodo1(unsigned int dimImagen)
+{
+    //este metodo genera 6*n^2 señales, siendo n = dimImagen
+    delete D;
+    D = new Matriz(6*dimImagen*dimImagen, dimImagen*dimImagen);
+
+    int fila = 0;
+    for(unsigned int i = 1; i < dimImagen; i++){
+        for(unsigned int j = 1; j <= dimImagen; j++){
+            //tiro las rectas de la pared izquierda
+            tirarSenal(0, i, j, 0, fila); //sobre el piso
+            tirarSenal(0, i, dimImagen, j, fila + 1); //sobre pared derecha
+            if(j != dimImagen)
+                tirarSenal(0, i, j, dimImagen, fila + 2); //sobre el techo
+
+            //tiro las rectas de la pared derecha
+            tirarSenal(dimImagen, i, j, 0, fila + 3);                 //sobre el piso
+            tirarSenal(dimImagen, i, 0, j - 1, fila + 4);             //sobre pared izquierda
+            if(j != dimImagen)
+                tirarSenal(dimImagen, i, j - 1, dimImagen, fila + 5); //sobre el techo
+
+            fila = fila + 6;
+        }
+    }
+}
 
 void Senales :: tirarSenal(long double x1, long double y1, long double x2, long double y2, int filaALlenar)
 {
-
     int n = (int)sqrt((long double)D->columnas()); //n guarda la dimension de la matriz de pixels
 
-    if (x1 - x2 == 0){   //si la senial es vertical hacemos las cuentas "a mano"
-                         //una senial vertical pasa por toda una columna de pixel con distancia, recorriendo
-                         //una distancia de 1 por cada uno
+    if (x1 == x2){   //si la senial es vertical hacemos las cuentas "a mano"
+                     //una senial vertical pasa por toda una columna de pixel con distancia, recorriendo
+                     //una distancia de 1 por cada uno
         for (int fil = 0; fil < n; fil++)
-            D->asignar(filaALlenar, fil*n + (int)x1, 1);
+            if (x1 == n)
+                D->asignar(filaALlenar, fil*n + n - 1, 1);
+            else
+                D->asignar(filaALlenar, fil*n + (int)x1, 1);
     }
     else{
         //primero construyo la recta
@@ -60,9 +93,18 @@ void Senales :: tirarSenal(long double x1, long double y1, long double x2, long 
         //          x2 - x1 != 0 paratodo x2, x1 reales, salvo que sea una senial vertical, lo cual tratamos anteriormente
         long double a = (y2 - y1) / (x2 - x1);
         long double b = y1 - x1*((y2 - y1) / (x2 - x1));
-        long double** pares;
-        pares = new long double* [2*(n+1)];   //2*(n+1) lo peor es que pase por la
-                                              //diagonal entonces va a haber 2*(n+1) pares
+        long double** pares = new long double* [2*(n+1)];
+        //2*(n+1) lo peor es que pase por la
+        //diagonal entonces va a haber 2*(n+1) pares;
+
+        long double yEntero;                  //para ver si hay pares repetidos
+        int y = 0;
+        bool* yRepetidos = new bool [n + 1];
+
+        for(int i = 0; i < n+1; i++){
+            yRepetidos[i] = false;
+        }
+
         for(int i = 0; i < 2*(n+1); i++){
             pares[i] = new long double [2];
         }
@@ -80,6 +122,13 @@ void Senales :: tirarSenal(long double x1, long double y1, long double x2, long 
             if((temp >= 0) && (temp <= n)){
                 pares[cantPares][0] = i;
                 pares[cantPares][1] = temp;
+
+                y = (int)temp;          //paso el valor a entero
+                yEntero = y;            //lo guardo para comparar
+
+                if (yEntero == temp){   //veo si es entero y si es guardo la coordenada y para no contarla mas adelante
+                    yRepetidos[y] = true;
+                }
                 cantPares++;
             }
         }
@@ -88,14 +137,20 @@ void Senales :: tirarSenal(long double x1, long double y1, long double x2, long 
             long double temp = (i - b)/a;
 
             if(temp >= 0 && temp <= n){
-                pares[cantPares][0] = temp;
-                pares[cantPares][1] = i;
-                cantPares++;
+                if(!yRepetidos[i]){
+                    pares[cantPares][0] = temp;
+                    pares[cantPares][1] = i;
+                    cantPares++;
+                }
             }
         }
 
         //ordeno por la primer componente
-        ordenarPares(pares, cantPares);
+//        if (a > 0)
+            ordenarParesMenorAMayor(pares, cantPares);
+//        else
+//            ordenarParesMayorAMenor(pares, cantPares);
+
 
     //    for(int i = 0; i < cantPares; i++){
     //        cout << "(" << pares[i][0] << "," << pares[i][1] << ")" << ", ";
@@ -113,7 +168,12 @@ void Senales :: tirarSenal(long double x1, long double y1, long double x2, long 
                 //siguiente a este es un valor valido(si no me pase de rango)
                 if( (pares[i+1][0] == -1) && (i+2 < cantPares) ){
                     //menor en x
-                    col = (int)pares[i][0];
+//                    if((int)pares[i][0] < (int)pares[i+2][0]){
+                        col = (int)pares[i][0];
+//                    }
+//                    else
+//                        col = (int)pares[i+1][0];
+
                     //menor en y, arreglo para coordenadas de la matriz
                     if((int)pares[i][1] < (int)pares[i+2][1]){
                         fil = n - 1 - (int)pares[i][1];
@@ -157,10 +217,11 @@ void Senales :: tirarSenal(long double x1, long double y1, long double x2, long 
             delete [] pares[i];
         }
         delete [] pares;
+        delete [] yRepetidos;
     }
 }
 
-void Senales :: ordenarPares(long double** pares, int filas){
+void Senales :: ordenarParesMenorAMayor(long double** pares, int filas){
     int topeInf = 0;
     long double temp1;  //variable temporal para el primer componente de la tupla
     long double temp2;  //variable temporal para el segundo componente de la tupla
@@ -194,18 +255,47 @@ void Senales :: ordenarPares(long double** pares, int filas){
     }
 }
 
+void Senales :: ordenarParesMayorAMenor(long double** pares, int filas){
+    int topeInf = 0;
+    long double temp1;  //variable temporal para el primer componente de la tupla
+    long double temp2;  //variable temporal para el segundo componente de la tupla
+
+
+    while (topeInf < (filas - 1)){
+        int i = topeInf;
+        int posMayor = i;
+        long double mayor = pares[i][0];
+
+        while (i < filas){
+            if(pares[i][0] > mayor){
+                mayor = pares[i][0];
+                posMayor = i;
+            }
+            i++;
+        }
+
+        /* hago el swap de los elementos */
+        temp1 = pares[topeInf][0];
+        temp2 = pares[topeInf][1];
+
+        pares[topeInf][0] = pares[posMayor][0];
+        pares[topeInf][1] = pares[posMayor][1];
+
+        pares[posMayor][0] = temp1;
+        pares[posMayor][1] = temp2;
+
+        topeInf++;
+
+    }
+}
+
 void Senales :: anularRepetidos(long double** pares, int cantPares){
     int cant = cantPares;
-    int i = 0;
 
-    while (i < (cant - 1)){
+    for (int i = 0; i < (cant - 1); i++) {
         if(pares[i][0] == pares[i+1][0]){
             pares[i+1][0] = -1;
-            i = i + 2;           //salteo el anulado
+            i++;            //salteo el anulado
         }
-        else
-           i++;
     }
-
-
 }
