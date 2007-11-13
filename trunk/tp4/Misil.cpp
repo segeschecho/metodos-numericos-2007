830d570ab61c53f2a35b19918d08cca6ae552435
hadeskleizer <hadeskleizer@85ae770f-1639-0410-9470-155aa9614729>
2007-11-13 05:30:46 +0000

    Ahora si, splines andando perfecto (eso espero)

--- trunk/tp4/Misil.cpp
+++ trunk/tp4/Misil.cpp
@@ -80,16 +80,46 @@ void Misil :: spline (const long double* muestra, long double* res)
     coeficientesC.triangular(&b);
     cout << coeficientesC << endl;
     cout << b << endl;
-*/
-    long double diag = (long double)4;    //diag = resultado de la diagonal correspondiente a c(n-1)
-    long double coef;    //auxiliar, para "restar fila1 - coef*fila2"
-    long double vectorB = 3*(muestra[2] - 2*muestra[1] + muestra[0]);    //el resultado correspondiente (del vector b)
 
-    for (int i = 2; i < cantMediciones - 1; i++) {
-        coef = (long double)1/diag;
-        diag = (long double)4 - coef;
-        vectorB = 3*(muestra[i + 1] - 2*muestra[i] + muestra[i - 1]) - coef*vectorB;
+    /*
+        La matriz para spline cubicos naturales es:
+        3 0 0 0 ... 0 0 0 | 0
+        1 4 1 0 ... 0 0 0 | 3*(a(2) - 2*a(1) + a(0))
+        0 1 4 1 ... 0 0 0 | 3*(a(3) - 2*a(2) + a(1))
+        .           .
+        .           .
+        .           .
+        0 0 0 0 ... 1 4 1 | 3*(a(n) - 2*a(n-1) + a(n-2))
+        0 0 0 0 ... 0 0 3 | 0
+
+        siendo a(i) el coeficiente 'a' del spline 'i' y n = cantMediciones
+
+        Al triangular desde la 2da fila hacia la anteultima mediante el metodo de gauss,
+        el coeficiente siempre es 1/diagonal. Luego diagonal = 4 - 1*coeficiente.
+        Solo necesitamos c(n-1), por lo que me interesa solamente el anteultimo valor de la diagonal.
+    */
+
+    long double diag;       //diag = resultado de la diagonal correspondiente a c(n-1)
+    long double vectorB;    //el resultado correspondiente (del vector b)
+
+    //CASO PARTICULAR: la matriz es de 2x2
+    if (cantMediciones == 2){
+        diag = 3;
+        vectorB = 0;
     }
+    else{
+        diag = 4;
+        vectorB = 3*(muestra[2] - 2*muestra[1] + muestra[0]);
+        long double coef;    //auxiliar, para "restar fila1 - coef*fila2"
+
+        for (int i = 2; i < cantMediciones - 1; i++) {
+            coef = 1/diag;
+            diag = 4 - coef;
+            vectorB = 3*(muestra[i + 1] - 2*muestra[i] + muestra[i - 1]) - coef*vectorB;
+        }
+    }
+
+    cout << diag;
 
     res[1] = vectorB/diag;
     //ya tengo el c(n-1), ahora necesito a(n-1), b(n-1) y d(n-1)
